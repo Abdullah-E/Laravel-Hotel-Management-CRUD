21ddfbf276137bf0be157d5cd467e2b1055f5773
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-07-24 20:35:12 +0500

    routes registered without credentials

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 class HotelController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth:api');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api');
+    // }
     public function index(Request $request)
     {
         $query = Hotel::query();
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
-use APP\Http\Controllers\HotelController;
+<?php
 
-Route::middleware('auth:api')->group(function () {
-    Route::get('/hotels', [HotelController::class, 'index']);
-    Route::post('/hotels', [HotelController::class, 'store']);
-    Route::get('/hotels/{id}', [HotelController::class, 'show']);
-    Route::put('/hotels/{id}', [HotelController::class, 'update']);
-    Route::delete('/hotels/{id}', [HotelController::class, 'destroy']);
-});
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HotelController;
+
+// Route::middleware('auth:api')->group(function () {
+Route::get('/hotels', [HotelController::class, 'index']);
+Route::post('/hotels', [HotelController::class, 'store']);
+Route::get('/hotels/{id}', [HotelController::class, 'show']);
+Route::put('/hotels/{id}', [HotelController::class, 'update']);
+Route::delete('/hotels/{id}', [HotelController::class, 'destroy']);
+// });
