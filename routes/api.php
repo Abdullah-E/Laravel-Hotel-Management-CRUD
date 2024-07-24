3c314e148d19f8ec758712d0ec1c9e365c401ded
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-07-24 22:26:36 +0500

    client credentials complete

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HotelController;
 
-// Route::middleware('auth:api')->group(function () {
-Route::get('/hotels', [HotelController::class, 'index']);
-Route::post('/hotels', [HotelController::class, 'store']);
-Route::get('/hotels/{id}', [HotelController::class, 'show']);
-Route::put('/hotels/{id}', [HotelController::class, 'update']);
-Route::delete('/hotels/{id}', [HotelController::class, 'destroy']);
-// });
+Route::middleware('client')->group(function () {
+    Route::get('/hotels', [HotelController::class, 'index']);
+    Route::post('/hotels', [HotelController::class, 'store']);
+    Route::get('/hotels/{id}', [HotelController::class, 'show']);
+    Route::put('/hotels/{id}', [HotelController::class, 'update']);
+    Route::delete('/hotels/{id}', [HotelController::class, 'destroy']);
+});
