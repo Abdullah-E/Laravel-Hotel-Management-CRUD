06c40253e1c22c2cbcfd42e237eb9017a20f506b
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-07-25 08:21:38 +0500

    controller working for facilities and all apis

--- laravel/app/Http/Controllers/HotelController.php
+++ laravel/app/Http/Controllers/HotelController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Hotel;
+use App\Models\Facility;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HotelController extends Controller
 {
@@ -14,44 +16,103 @@ class HotelController extends Controller
     // }
     public function index(Request $request)
     {
-        $query = Hotel::query();
+        $request->validate([
+            'sort_by' => 'string|in:Hotel Name,Country,City,Price'
+        ]);
+        $sortBy = $request->input('sort_by', 'Hotel Name');
+        return Hotel::with('facilities')->get()->map(function ($hotel){
 
-        if ($request->has('sort')) {
-            $sort = $request->get('sort');
-            $query->orderBy($sort);
-        }
+            $hotel->facilities = $hotel->facilities->transform(function ($facility) {
+                return $facility->{"Facility Name"};
+            })->toArray();
 
-        return $query->get();
+
+            return $hotel;
+        })->sortBy($sortBy)->values();
+
+
     }
 
     public function store(Request $request)
     {
-        $hotel = Hotel::create($request->all());
-
+        $request->validate([
+            'Hotel Name' => 'required|string',
+            'Country' => 'required|string',
+            'City' => 'required|string',
+            'Price' => 'required|numeric',
+            'facilities' => 'array'
+        ]);
+        $hotel = DB::transaction(function () use ($request) {
+            $hotel = Hotel::create($request->except('facilities'));
+    
+            if ($request->has('facilities')) {
+                $facilities = $request->input('facilities');
+                $facilityIds = [];
+    
+                foreach ($facilities as $facility) {
+                    $facilityIds[] = Facility::firstOrCreate(['Facility Name' => $facility])->id;
+                }
+    
+                $hotel->facilities()->sync($facilityIds);
+            }
+    
+            return $hotel->load('facilities');
+        });
+    
         return response()->json($hotel, 201);
+
     }
 
     public function show($id)
     {
-        $hotel = Hotel::find($id);
+
+        $hotel = Hotel::with('facilities')->find($id);
 
         if (!$hotel) {
             return response()->json(['message' => 'Hotel not found'], 404);
         }
 
-        return $hotel;
+        $hotel->facilities = $hotel->facilities->transform(function ($facility) {
+            return $facility->{"Facility Name"};
+        })->toArray();
+
+        return response()->json($hotel);
     }
 
     public function update(Request $request, $id)
     {
-        $hotel = Hotel::findOrFail($id);
-        $hotel->update($request->all());
-        return response()->json($hotel, 200);
+        $request->validate([
+            'Hotel Name' => 'string',
+            'Country' => 'string',
+            'City' => 'string',
+            'Price' => 'numeric',
+            'facilities' => 'array'
+        ]);
+        $updatedHotel = DB::transaction(function () use ($request, $id) {
+            $hotel = Hotel::findOrFail($id);
+            $hotel->update($request->except('facilities'));
+
+            if ($request->has('facilities')) {
+                $facilities = $request->input('facilities');
+                $facilityIds = [];
+
+                foreach ($facilities as $facilityName) {
+                    $facilityIds[] = Facility::firstOrCreate(['name' => $facilityName])->id;
+                }
+
+                $hotel->facilities()->sync($facilityIds);
+            }
+
+            return $hotel->load('facilities');
+        });
+        return response()->json($updatedHotel, 200);
+        
     }
 
     public function destroy($id)
     {
+
         Hotel::findOrFail($id)->delete();
-        return response()->json(['message' => 'Hotel deleted successfully'], 204);
+        return response()->json(['message' => 'Hotel deleted successfully'], 200);
     }
 }
